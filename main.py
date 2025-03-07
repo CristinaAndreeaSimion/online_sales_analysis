4a61b46176771024849ad4dcaafa23e3316a442e
Cristina <c090192@gmail.com>
2025-03-07 10:32:18 +0200

    Added product removal functionality

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ from product_manager import ProductManager
 if __name__ == "__main__":
     manager = ProductManager()
     
-    product1 = Product("Laptop", 1500, 10)
-    product2 = Product("Mouse", 50, 100)
+    product1 = Product("Pc", 1500, 10)
+    product2 = Product("Mouse_wireless", 50, 100)
     product3 = Product("Keyboard", 80, 50)
     
-    manager.add_product(product1)
-    manager.add_product(product2)
-    manager.add_product(product3)
-    
+
     print("All Products:")
-    manager.display_products()
-    print(f"Total Inventory Value: {manager.total_inventory_value()}")
+    
+   
 
 
     
